7d5443869524dcc275b815aef0b193812e9db7ef
cruzcruz <cjcb040404@hotmail.com>
2023-11-29 12:46:26 -0600

    Initial commit

---
+++ app/src/main/java/com/example/classtovmm/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.classtovmm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText editTextNum1, editTextNum2;
+    private TextView textViewResultado;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editTextNum1 = findViewById(R.id.editTextNum1);
+        editTextNum2 = findViewById(R.id.editTextNum2);
+        textViewResultado = findViewById(R.id.textViewResultado);
+    }
+
+    public void sumar(View view) {
+        // Obtener los valores de los EditText
+        String strNum1 = editTextNum1.getText().toString();
+        String strNum2 = editTextNum2.getText().toString();
+
+        // Verificar que se ingresaron números
+        if (!strNum1.isEmpty() && !strNum2.isEmpty()) {
+            // Convertir los valores a números
+            double num1 = Double.parseDouble(strNum1);
+            double num2 = Double.parseDouble(strNum2);
+
+            // Realizar la suma
+            double resultado = num1 + num2;
+
+            // Mostrar el resultado en el TextView
+            textViewResultado.setText("Resultado: " + resultado);
+        } else {
+            // Manejar el caso en que no se ingresaron números
+            textViewResultado.setText("Ingrese números en ambos campos");
+        }
+    }
+}
